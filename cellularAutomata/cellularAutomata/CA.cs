f29d9bf01bdc104d2cb87b9f9e37467708c8589f
Joshua Arber <arber@unlv.nevada.edu>
2020-05-31 12:08:59 -0700

    initial commit

---
+++ cellularAutomata/cellularAutomata/CA.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cellularAutomata
+{
+    class CA
+    {
+        int[] ruleSet = { 0, 1, 0, 1, 1, 0, 1, 0 };
+        int[] cells;
+        int cellSize;
+        int generation = 0;
+
+        public CA(int size, int width)
+        {            
+            cells = new int[width];
+
+            for (int i = 0; i < cells.Length; i++)
+            {
+                cells[i] = 0;
+            }
+
+            cells[cells.Length / 2] = 1;
+            cellSize = size;
+        }
+
+        public void generate()
+        {
+            int[] nextGen = new int[cells.Length];
+
+            for (int i = 1; i < cells.Length - 1; i++)
+            {
+                int left = cells[i - 1];
+                int mid = cells[i];
+                int right = cells[i + 1];
+
+                nextGen[i] = rules(left, mid, right);
+            }
+
+            cells = nextGen;
+            generation++;
+        }
+
+        private int rules(int a, int b, int c)
+        {
+            int[] bits = { a, b, c };
+
+            // 2^2 + 2^1 + 2^0
+            int index = 0;
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                index = (index << 1) | bits[i];
+            }
+
+            //Console.Write(a + ", " + b + ", " + c + " = ");
+            //Console.WriteLine(index);
+            return ruleSet[index];
+        }
+        
+        public Bitmap drawCA(Bitmap bm)
+        {
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                Color c;
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    if (cells[i] == 1)
+                    {
+                        c = Color.Black;
+                    }
+                    else
+                    {
+                        c = Color.White;
+                    }
+                    
+                    Rectangle r = new Rectangle(i * cellSize, generation * cellSize, cellSize, cellSize);
+                    g.DrawRectangle(new Pen(c), r);
+                    g.FillRectangle(new SolidBrush(c), r);
+                }
+            }
+
+            return bm;
+        }
+    }
+}
---
+++ cellularAutomata/cellularAutomata/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cellularAutomata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int cellSize = 10;
+            int generations = 40;
+
+            Bitmap bm = new Bitmap(Canvas.Width, Canvas.Height);            
+            CA ca = new CA(cellSize, Canvas.Width / cellSize);
+
+            for (int i = 0; i < generations; i++)
+            {
+                Canvas.Image = ca.drawCA(bm);
+                ca.generate();
+            }
+        }
+
+
+    }
+}
